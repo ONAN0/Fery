7ffafe1559382808ce86d70d56c829f95636f288
NAN0 <135762196+ONAN0@users.noreply.github.com>
2023-11-18 18:24:45 +0100

    6 - react-hook-forum

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,65 @@
-import { FC } from 'react';
-import Button from './components/Button';
-import './components/Button.css';
+import { useForm, SubmitHandler } from 'react-hook-form';
 
-const handleClick = () => {
-  console.log('Finally.');
-};
-
-const App: FC = () => {
-  return (
-    <div className="App">
-      <div className="Button">
-        <h1>Čauko Fery</h1>
-        <Button buttonText="Click me" onClick={handleClick} />
-      </div>
-    </div>
-  );
+interface FormData {
+  meno: string;
+  priezvisko: string;
+  email: string;
+  sprava: string;
 }
 
-export default App;
+const MyForm: React.FC = () => {
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+
+  } = useForm<FormData>();
+
+  const onSubmit: SubmitHandler<FormData> = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <div>
+      <h1>Sorry Fery za čakanie</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+
+        <label>
+          Meno:
+          <input {...register('meno', { required: 'Meno je povinné' })} />
+          {errors.meno && <span>{errors.meno.message}</span>}
+        </label>
+
+        <label>
+          Priezvisko:
+          <input {...register('priezvisko', { required: 'Priezvisko je povinné' })} />
+          {errors.priezvisko && <span>{errors.priezvisko.message}</span>}
+        </label>
+
+        <label>
+          Email:
+          <input
+            {...register('email', {
+              required: 'Email je povinný',
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                message: 'Neplatný email',
+              },
+            })}
+          />
+          {errors.email && <span>{errors.email.message}</span>}
+        </label>
+
+        <label>
+          Správa:
+          <textarea {...register('sprava')} />
+        </label>
+
+        <button type="submit">Odoslať</button>
+      </form>
+    </div>
+  );
+};
+
+export default MyForm;
